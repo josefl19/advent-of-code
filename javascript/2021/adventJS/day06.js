e08307fdbd08c93971868f2c61b2c0fec5710a35
josefl19 <jose.fl_@hotmail.com>
2021-12-07 13:58:52 -0600

    day06 complete

---
+++ javascript/2021/adventJS/day06.js
@@ -0,0 +1,43 @@
+/**
+ * Antes de poder disfrutar de la navidad... nos toca terminar de rematar los exámenes finales. ¡Y toca un poco de matemáticas! 😱
+ * 
+ * A una función se le pasan dos parámetros: un Array con números y el resultado que se espera.
+ * La función debe devolver los dos valores del Array que sumen el resultado esperado. Como a veces pueden haber más de dos valores 
+ * que sumen, se devolverá el primero empezando por la izquierda que encuentre otro par, sin importar lo lejos que esté a la derecha.
+ * 
+ * Si no se encuentra, se devuelve null. Veamos unos ejemplos:
+ * 
+ * sumPairs([3, 5, 7, 2], 10) // [3, 7]
+ * sumPairs([-3, -2, 7, -5], 10) // null
+ * sumPairs([2, 2, 3, 1], 4) // [2, 2]
+ * sumPairs([6, 7, 1, 2], 8) // [6, 2]
+ * sumPairs([0, 2, 2, 3, -1, 1, 5], 6) // [1, 5]
+ * 
+ * El resultado tiene que ser un array con dos números.
+ * 
+ * Una vez que tengas el resultado... ¿cómo podrías hacer que fuese lo más óptimo posible para no tener que recorrer las mismas situaciones dos veces 🤔?
+ */
+
+function sumPairs(numbers, result) {
+    // ¡Y no olvides compartir tu solución en redes!
+    var par = []
+    for (var i = 0; i < numbers.length; i++) {
+        for (var j = i + 1; j <= numbers.length; j++) {
+            if ((numbers[i] + numbers[j]) == result) {
+                par.push(numbers[i], numbers[j])
+                break
+            }
+        }
+
+        if (par.length > 0) { break }
+    }
+
+    (par.length == 0) ? par = null: par
+    return par
+}
+
+console.log(sumPairs([3, 5, 7, 2], 10)) // [3, 7]
+console.log(sumPairs([-3, -2, 7, -5], 10)) // null
+console.log(sumPairs([2, 2, 3, 1], 4)) // [2, 2]
+console.log(sumPairs([6, 7, 1, 2], 8)) // [6, 2]
+console.log(sumPairs([0, 2, 2, 3, -1, 1, 5], 6)) // [1, 5]
