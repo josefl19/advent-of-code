42823160a5b0866dcae529b12501bfb8c12a8aad
josefl <jose.fl_@hotmail.com>
2022-08-14 00:02:12 -0500

    descripcion de dia 9 y solucion añadida

---
+++ javascript/2021/adventJS/day09/day09.js
@@ -0,0 +1,34 @@
+function groupBy(collection, it) {
+    // ¡No olvides compartir tu solución en redes!
+    let obj = {}
+    let metodo = typeof it === 'function'
+    
+    for(const value of collection) {
+      let key = metodo ? it(value) : value[it]
+      
+      obj[key] ??= []
+      obj[key].push(value)
+    }
+    
+    return obj
+}
+
+// Pruebas
+groupBy([6.1, 4.2, 6.3], Math.floor) // { 6: [6.1, 6.3], 4: [4.2] }
+groupBy(['one', 'two', 'three'], 'length') // { 3: ['one', 'two'], 5: ['three'] }
+groupBy([{age: 23}, {age: 24}], 'age') // { 23: [{age: 23}], 24: [{age: 24}] }
+
+groupBy(
+  [1397639141184, 1363223700000],
+  timestamp => new Date(timestamp).getFullYear()
+)
+// { 2013: [1363223700000], 2014: [1397639141184] }
+
+groupBy([
+  { title: 'JavaScript: The Good Parts', rating: 8 },
+  { title: 'Aprendiendo Git', rating: 10 },
+  { title: 'Clean Code', rating: 9 },
+], 'rating')
+// { 8: [{ title: 'JavaScript: The Good Parts', rating: 8 }],
+//   9: [{ title: 'Clean Code', rating: 9 }],
+//   10: [{ title: 'Aprendiendo Git', rating: 10 }] }
