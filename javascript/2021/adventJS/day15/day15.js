bbf85d84537c236b1c5366731f4a54c3d3e05c07
josefl <jose.fl_@hotmail.com>
2022-08-15 22:00:27 -0500

    descripcion de dia 15 y cambio de solucion hacia dentro de carpeta

--- javascript/2021/adventJS/day15/day15.js
+++ javascript/2021/adventJS/day15/day15.js
@@ -1,20 +1,3 @@
-/**
- * ¡Estamos haciendo los últimos ajustes para el trineo de Santa Claus!
- * 
- * Como ya sabes, el trineo es volador y estamos ajustando el motor para que haga parabolas lo más óptimas posibles. 
- * Para esto el salto debe ser siempre hacia arriba y, a partir del punto más alto, debe bajar siempre hacia abajo...
- * 
- * Nuestro mecánico de confianza, Kiko Belfs, que tiene un Tesla genial, nos ha explicado que los saltos se pueden ver 
- * como arrays... y que sólo tenemos que asegurarnos que los números suben y bajan de forma correcta. También nos avisa 
- * que sólo pasaremos arrays de, como mínimo, tres posiciones.
- * 
- * Nos ha pasado algunos ejemplos de cómo debería ser nuestra función y algunos resultados:
- * 
- * Lo importante: recorrer el array de izquierda a derecha para ver que la subida es siempre estricta, detectar el punto 
- * más alto y entonces ver que la bajada es estricta hacia abajo...
- * 
- */
-
 function checkSledJump(heights) {
     var optimo = true;
     // Index de la altura máxima
@@ -36,6 +19,7 @@ function checkSledJump(heights) {
     return optimo;
 }
 
+// Pruebas
 console.log(checkSledJump([1, 2, 3, 2, 1])); // true: sube y baja de forma estricta
 console.log(checkSledJump([0, 1, 0])); // -> true: sube y baja de forma estricta
 console.log(checkSledJump([0, 3, 2, 1])); // -> true: sube y baja de forma estricta
