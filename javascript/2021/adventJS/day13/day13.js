c39d8459987b4e9464398432ff41f90f8db42863
josefl <jose.fl_@hotmail.com>
2022-08-15 21:50:32 -0500

    descripcion de dia 13 y cambio de solucion hacia dentro de carpeta

--- javascript/2021/adventJS/day13/day13.js
+++ javascript/2021/adventJS/day13/day13.js
@@ -1,15 +1,3 @@
-/**
- * ¡Hay demasiados regalos 🎁! Y envolverlos es una locura...
- * 
- * Vamos a crear una función que pasándole un array de regalos, nos devuelva otro array pero donde 
- * todos los regalos han sido envueltos con asteriscos tanto por arriba como por los lados.
- * 
- * Sólo tienes que tener en cuenta unas cosillas ✌️:
- *      - Si el array está vacío, devuelve un array vacío
- *      - Los regalos son emojis 🎁... por lo que tenlo en cuenta a la hora de contar su longitud...
- *      - Por suerte, cada posición del array siempre tiene la misma longitud...
- */
-
 function wrapGifts(gifts) {
     var tamanos = [];
     var cuerpo = [];
@@ -23,6 +11,7 @@ function wrapGifts(gifts) {
     return [base, ...cuerpo, base];
 }
 
+// Pruebas
 console.log(wrapGifts(["📷", "⚽️"]));
 /* Resultado:
 [ '****',
