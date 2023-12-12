428120843094981b16c906ac9bc023d404f7041d
josefl <jose.fl_@hotmail.com>
2023-12-11 19:09:49 -0600

    Day01 solution

--- javascript/2023/adventJS/lib/day01-primer-regalo-repetido/day01.js
+++ javascript/2023/adventJS/lib/day01-primer-regalo-repetido/day01.js
@@ -1,32 +1,19 @@
-function findFirstRepeated(gifts) {
-    let uniquesIds = [ ... new Set(gifts) ];
-    let 
-
-    if( uniquesIds.length == gifts.length ) return -1
-
-    uniquesIds.forEach((element, index, gifts) => {
-        let ocurrencias = [];
-    });
-
-
-
-    console.log(uniquesIds);
-
-
-    return 0
+class Day01 {
+    static findFirstRepeated(gifts) {
+        let firstIndexRepeated = -1;
+    
+        gifts.forEach((g, index) => {
+            let subGifts = gifts.slice(index+1);
+            let elementRepeated = subGifts.indexOf(g) != -1 ? subGifts.indexOf(g) + (index + 1) : -1;
+    
+            if(firstIndexRepeated == -1 && elementRepeated != -1) firstIndexRepeated = elementRepeated;
+            if(elementRepeated != -1) {
+                if(elementRepeated < firstIndexRepeated) firstIndexRepeated = elementRepeated
+            }
+        });
+     
+        return firstIndexRepeated != -1 ? gifts[firstIndexRepeated] : -1;
+    }
 }
 
-const giftIds = [2, 1, 3, 5, 3, 2]
-const firstRepeatedId = findFirstRepeated(giftIds)
-console.log(firstRepeatedId) // 3
-// Aunque el 2 y el 3 se repiten
-// el 3 aparece primero por segunda vez
-
-const giftIds2 = [1, 2, 3, 4]
-const firstRepeatedId2 = findFirstRepeated(giftIds2)
-console.log(firstRepeatedId2) // -1
-// Es -1 ya que no se repite ningún número
-
-const giftIds3 = [5, 1, 5, 1]
-const firstRepeatedId3 = findFirstRepeated(giftIds3)
-console.log(firstRepeatedId3) // 5
+module.exports = Day01;
--- javascript/2023/adventJS/test/day01.test.js
+++ javascript/2023/adventJS/test/day01.test.js
@@ -1,6 +1,69 @@
+const Day01 = require("./../lib/day01-primer-regalo-repetido/day01")
 
 describe("Test Day01 - Primer regalo repedito", () => {
-    test("", () => {
-        
+    test("Test 01 - Return type", () => {
+        const day01 = Day01.findFirstRepeated([2, 1, 3, 5, 3, 2]);
+        expect(typeof day01).not.toBeUndefined();
+        expect(typeof day01).toBe("number");
+    });
+
+    test("Test 02", () => {
+        const day01 = Day01.findFirstRepeated([2, 1, 3, 5, 3, 2]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(3);
+    });
+
+    test("Test 03", () => {
+        const day01 = Day01.findFirstRepeated([2, 2]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(2);
+    });
+
+    test("Test 04", () => {
+        const day01 = Day01.findFirstRepeated([2, 4, 3, 5, 1]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(-1);
+    });
+
+    test("Test 05", () => {
+        const day01 = Day01.findFirstRepeated([1, 3, 4, 5, 0, 1, 3, 0, 7]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(1);
+    });
+
+    test("Test 06", () => {
+        const day01 = Day01.findFirstRepeated([]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(-1);
+    });
+
+    test("Test 07", () => {
+        const day01 = Day01.findFirstRepeated([10, 20, 30]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(-1);
+    });
+
+    test("Test 08", () => {
+        const day01 = Day01.findFirstRepeated([5, 1, 2, 3, 2, 5, 1]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(2);
+    });
+
+    test("Test 09", () => {
+        const day01 = Day01.findFirstRepeated([1, 10, 2, 10, 20, 50, 7, 0, 0, 7]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(10);
+    });
+
+    test("Test 10 - Test in description", () => {
+        const day01 = Day01.findFirstRepeated([1, 2, 3, 4]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(-1);
+    });
+
+    test("Test 11 - Test in description", () => {
+        const day01 = Day01.findFirstRepeated([5, 1, 5, 1]);
+        expect(day01).not.toBeUndefined();
+        expect(day01).toBe(5);
     });
 });
