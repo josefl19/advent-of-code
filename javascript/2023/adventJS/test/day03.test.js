b3d97a13a0be7bbb914f7bf4351f85ecd0e324b8
josefl <jose.fl_@hotmail.com>
2023-12-11 19:14:29 -0600

    Day03 solution

--- javascript/2023/adventJS/test/day03.test.js
+++ javascript/2023/adventJS/test/day03.test.js
@@ -7,38 +7,38 @@ describe("Tests Day03 - El elfo travieso", () => {
     });
 
     test("Test #02", () => {
-        const day02_test02 = Day03.findNaughtyStep('abcd', 'abcde');
-        expect(typeof day02_test02).toBe("string");
-        expect(day02_test02).toEqual('e');
+        const day03_test02 = Day03.findNaughtyStep('abcd', 'abcde');
+        expect(typeof day03_test02).toBe("string");
+        expect(day03_test02).toEqual('e');
     });
 
     test("Test #03", () => {
-        const day02_test02 = Day03.findNaughtyStep('abcde', 'abcd');
-        expect(typeof day02_test02).toBe("string");
-        expect(day02_test02).toEqual('e');
+        const day03_test02 = Day03.findNaughtyStep('abcde', 'abcd');
+        expect(typeof day03_test02).toBe("string");
+        expect(day03_test02).toEqual('e');
     });
 
     test("Test #04", () => {
-        const day02_test02 = Day03.findNaughtyStep('xxxx', 'xxoxx');
-        expect(typeof day02_test02).toBe("string");
-        expect(day02_test02).toEqual('o');
+        const day03_test02 = Day03.findNaughtyStep('xxxx', 'xxoxx');
+        expect(typeof day03_test02).toBe("string");
+        expect(day03_test02).toEqual('o');
     });
 
     test("Test #05", () => {
-        const day02_test03 = Day03.findNaughtyStep('stepfor', 'stepor');
-        expect(typeof day02_test03).toBe("string");
-        expect(day02_test03).toEqual('f');
+        const day03_test03 = Day03.findNaughtyStep('stepfor', 'stepor');
+        expect(typeof day03_test03).toBe("string");
+        expect(day03_test03).toEqual('f');
     });
 
     test("Test #06", () => {
-        const day02_test03 = Day03.findNaughtyStep('iiiii', 'iiiii');
-        expect(typeof day02_test03).toBe("string");
-        expect(day02_test03).toEqual('');
+        const day03_test03 = Day03.findNaughtyStep('iiiii', 'iiiii');
+        expect(typeof day03_test03).toBe("string");
+        expect(day03_test03).toEqual('');
     });
 
     test("Test #04 - Test in description problem", () => {
-        const day02_test04 = Day03.findNaughtyStep('abcde', 'abcde');
-        expect(typeof day02_test04).toBe("string");
-        expect(day02_test04).toEqual('');
+        const day03_test04 = Day03.findNaughtyStep('abcde', 'abcde');
+        expect(typeof day03_test04).toBe("string");
+        expect(day03_test04).toEqual('');
     });
 });
